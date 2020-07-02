69aedb6aaacb15796a8e89000c2d4c4a199a766b
AndyZang-Salz <kaiyuezang@gmail.com>
2020-07-03 00:14:14 +0800

    Create Python Script.py

---
+++ .idea/fileTemplates/internal/Python Script.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : ${NAME}
+@Version : ??
+@Author  : Andy Zang
+@software: ${PRODUCT_NAME}
+@For     : 
+---------------------------------------
+"""
+
+# History:
+# ${DATE}: Create
+
+
+
+if __name__ == '__main__':
+    pass
