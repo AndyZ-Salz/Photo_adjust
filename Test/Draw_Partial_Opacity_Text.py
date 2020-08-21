dde08b53e7c8c99d11e9b523cd323beaf21cadb1
AndyZang-Salz <kaiyuezang@gmail.com>
2020-08-21 15:49:14 +0800

    Create Draw_Partial_Opacity_Text.py

---
+++ Test/Draw_Partial_Opacity_Text.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : Draw_Partial_Opacity_Text
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : 
+---------------------------------------
+"""
+
+# History:
+# 2020/8/21: Create
+from PIL import Image, ImageDraw, ImageFont
+
+
+def demo():
+    # get an image
+    base = Image.open("Pillow/Tests/images/hopper.png").convert("RGBA") #TODO need to download
+
+    # make a blank image for the text, initialized to transparent text color
+    txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
+
+    # get a font
+    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 40)
+    # get a drawing context
+    d = ImageDraw.Draw(txt)
+
+    # draw text, half opacity
+    d.text((10, 10), "Hello", font=fnt, fill=(255, 255, 255, 128))
+    # draw text, full opacity
+    d.text((10, 60), "World", font=fnt, fill=(255, 255, 255, 255))
+
+    out = Image.alpha_composite(base, txt)
+
+    out.show()
+
+
+if __name__ == '__main__':
+    demo()
