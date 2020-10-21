c69c8859b4db69cfa3e73d68c7bea7c22014a30a
AndyZang-Salz <kaiyuezang@gmail.com>
2020-10-21 09:09:38 +0800

    Create Pilliow_text_size_box_length.py
    test for pillow 8.0's new feature

---
+++ Test/Pilliow_text_size_box_length.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------
+@file    : Pilliow_text_size_box_length
+@Version : ??
+@Author  : Andy Zang
+@software: PyCharm
+@For     : to compare ImageDraw.Draw.textsize & ImageDraw.Draw.textlength & ImageDraw.Draw.textbbox
+---------------------------------------
+"""
+
+# History:
+# 2020/10/21: Create
+
+
+if __name__ == '__main__':
+    # prepare
+
