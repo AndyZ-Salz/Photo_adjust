0435477d86ac349573dc7dbdbad335351da0c9a7
AndyZang-Salz <kaiyuezang@gmail.com>
2020-12-08 15:57:26 +0800

    Update TextPosition.py
    a test case

--- Test/TextPosition.py
+++ Test/TextPosition.py
@@ -9,7 +9,6 @@
 ---------------------------------------
 """
 
-
 # Basically refer to the CSS's Box Model
 # Margin for position
 # Align for content
@@ -17,17 +16,19 @@
 # History:
 # 2020/12/8: Create
 
+from PIL import ImageDraw
+
 
 class TextPosition:
     # x_type must be "l", "m" or "r"
     # y_type must be "t", "m" or "b"
     # align must be "left", "center" or "right"
 
-    x_type = None,
+    x_type = "",
     x_margin = 0,
-    y_type = None,
+    y_type = "",
     y_margin = 0,
-    content_align = None
+    content_align = ""
 
     def __init__(self,
                  x_type=None,
@@ -81,12 +82,64 @@ class TextPosition:
 
     def position(self, base_img, text_body, text_font):
         x_total, y_total = base_img.size
+        output_x, output_y = 0, 0
+
+        return output_x, output_y
 
     def __str__(self):
         attr_str = ",".join("{}={}".format(k, self.__getattribute__(k)) for k in self.__dict__.keys())
-        return "Setting for position is {" + attr_str + "}"
+        return "Setting for text position is {" + attr_str + "}"
 
 
 if __name__ == '__main__':
-    xy = TextPosition("l",10,"b",content_align="center")
+    xy = TextPosition("l", 10, content_align="center")
     print(xy)
+
+    from PIL import Image, ImageDraw, ImageFont
+
+
+    def test_case():
+        # prepare
+        # set text body
+        text_body = "LINE1:ABQJKP\nline2:abqjkp"
+
+        # get an image
+        base_img = Image.new("RGBA", (500, 200), (255, 255, 255, 255))
+
+        # make a blank image for the text, initialized to transparent text color
+        text_layer = Image.new("RGBA", base_img.size, (255, 255, 255, 0))
+
+        # get a font
+        font_size = 40
+        font_name = "font/FreeMono.ttf"
+
+        text_font = ImageFont.truetype(font_name, font_size)
+
+        # set text color
+        text_color = (0, 0, 0, 255)
+
+        # get a drawing context
+        draw_obj = ImageDraw.Draw(text_layer)
+
+        text_position = TextPosition()
+        text_xy = text_position.position(base_img, text_body, text_font)
+
+        print("text_xy=", text_xy)
+
+        # visualization master box
+        draw_obj.rectangle([(0, 0), (base_img.size[0] - 1, base_img.size[1] - 1)], outline=(25, 25, 25, 255))
+
+        # bbox
+        text_bbox = draw_obj.textbbox(text_xy, text_body, font=text_font)
+        draw_obj.rectangle(text_bbox, fill=(202, 205, 205, 255))
+        print("text_bbox:", text_bbox)
+
+        # text
+        draw_obj.text(text_xy, text_body, font=text_font, fill=text_color, align="left")
+
+        # finish
+        out = Image.alpha_composite(base_img, text_layer)
+        out.show()
+
+
+    test_case()
